bab192cbea0625a2f32e956293fb3a09e382a4f4
kostasl <costaslag@gmail.com>
2012-11-30 12:48:39 +0000

    Added Camera Hopfield Demo and Noddy Ufilter code for complete project

---
+++ CameraDemo/CameraDemo.h
@@ -0,0 +1,16 @@
+/*
+ * CameraDemo.h
+ *
+ *  Created on: 3 Mar 2011
+ *      Author: kostasl
+ */
+
+#ifndef CAMERADEMO_H_
+#define CAMERADEMO_H_
+
+
+//Function Prototypes
+void showCameraInput();
+
+extern bool bRecallInProgress;
+#endif /* CAMERADEMO_H_ */
---
+++ Noddy/PerceptronCascade.h
@@ -0,0 +1,12 @@
+/*
+ * PerceptronCascade.h
+ *
+ *  Created on: 22 Mar 2011
+ *      Author: kostasl
+ */
+
+#ifndef PERCEPTRONCASCADE_H_
+#define PERCEPTRONCASCADE_H_
+
+
+#endif /* PERCEPTRONCASCADE_H_ */
