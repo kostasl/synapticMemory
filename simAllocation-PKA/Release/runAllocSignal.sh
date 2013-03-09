079e018c90d756681ea74c35a8aaa737ff5ad491
kostasl <costaslag@gmail.com>
2013-03-09 15:18:07 +0000

    Made PKA sim Sh runscript to run in background

--- simAllocation-PKA/Release/runAllocSignal.sh
+++ simAllocation-PKA/Release/runAllocSignal.sh
@@ -1,1 +1,10 @@
-./simAllocation-PKA --simulation=AllocSignalVsRepetitionTime --model=synapseSingleFilterUnifiedWithDecay --trials=102 --startSize=7 --endSize=13 --synapsesSize=10000 --initPeriod=0 --cSimTimeSecs=1000 --Timestep=1.00 --repPatIndex=0 --repPatCount=2 --AllocRefrac=1 --metaSampleTime=0 --cAMPDecay=0.5 --PKAAllocThres=0.2
+#!/bin/sh
+echo "Filter Size $1 To $2 Repetitions : $3 TRIALS: ${6:-1000}"
+echo "cAMP Decay $4"
+echo "PKA AllocThreshold $5"
+logc="Release/simOut$1-${2:?NoSize}R${3:?NoReps}F${4:?NoFc}P${5:?NoPKAThres}.log"
+
+echo "std output in $logc"
+cd ../
+nohup ./Release/simAllocation-PKA --simulation=AllocSignalVsRepetitionTime --model=synapseSingleFilterUnifiedWithDecay --trials=${6:-1000} --startSize=$1 --endSize=$2 --synapsesSize=10001 --initPeriod=0 --cSimTimeSecs=1000 --Timestep=1.00 --repPatIndex=0 --repPatCount=$3 --AllocRefrac=1 --metaSampleTime=0 --cAMPDecay=$4 --PKAAllocThres=$5 > $logc 2>&1 &
+tail -f $logc
